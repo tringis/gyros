a6e9909d18f585f704dacb20fc6f5da1203677aa
Tobias Ringström <tobias@ringis.se>
2008-08-27 20:59:50 +0000

    Add a convenience include file that includes all functionality.

---
+++ include/gyros/gyros.h
@@ -0,0 +1,9 @@
+#ifndef INCLUDED__gyros_gyros_h__200808261900
+#define INCLUDED__gyros_gyros_h__200808261900
+
+#include <gyros/task.h>
+#include <gyros/sleep.h>
+#include <gyros/mutex.h>
+#include <gyros/cond.h>
+
+#endif
