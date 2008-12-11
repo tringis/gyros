9026ed0e9feca7b1d876410c7fc1fe62c2acca3e
Tobias Ringström <tobias@ringis.se>
2008-12-11 09:10:23 +0000

    Add files missed in last commit.

---
+++ src/debug.c
@@ -0,0 +1,51 @@
+/**************************************************************************
+ * Copyright (c) 2002-2008, Tobias Ringström <tobias@ringis.se>
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ *
+ *    - Redistributions of source code must retain the above copyright
+ *      notice, this list of conditions and the following disclaimer.
+ *
+ *    - Redistributions in binary form must reproduce the above copyright
+ *      notice, this list of conditions and the following disclaimer in
+ *      the documentation and/or other materials provided with the
+ *      distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+ * A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+ * OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+ * SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+ * LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+ * DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+ * THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ **************************************************************************/
+#include <gyros/debug.h>
+#include <gyros/target/interrupt.h>
+
+static void (*s_handler)(const char *msg);
+
+void
+gyros_set_error_handler(void (*handler)(const char *msg))
+{
+    unsigned long flags = gyros_interrupt_disable();
+
+    s_handler = handler;
+    gyros_interrupt_restore(flags);
+}
+
+void
+gyros_error(const char *msg)
+{
+    if (s_handler)
+        s_handler(msg);
+
+    for (;;)
+        ;
+}
